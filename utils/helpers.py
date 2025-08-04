ef81029e7cc39cfc3f63b2fd25abeabdb2f71cbd
HeyGuys-source <jurassicdev500@gmail.com>
2025-08-04 03:34:58 +0100

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,17 @@
 import discord
+from datetime import datetime
 
-def create_success_embed(title: str, description: str) -> discord.Embed:
-    embed = discord.Embed(title=title, description=description, color=discord.Color.green())
+def create_embed(title: str, description: str = None, color: discord.Color = discord.Color.blurple()) -> discord.Embed:
+    embed = discord.Embed(title=title, color=color)
+    if description:
+        embed.description = description
     return embed
+
+def create_success_embed(message: str) -> discord.Embed:
+    return discord.Embed(description=message, color=discord.Color.green())
+
+def create_error_embed(message: str) -> discord.Embed:
+    return discord.Embed(description=message, color=discord.Color.red())
+
+def format_relative_time(dt: datetime) -> str:
+    return f"<t:{int(dt.timestamp())}:R>"
