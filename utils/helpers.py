04b68106481f6d6a7be858a7e511642514b7bf12
HeyGuys-source <jurassicdev500@gmail.com>
2025-08-04 03:16:01 +0100

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,5 @@
+import discord
 
+def create_embed(title: str, description: str, color: discord.Color = discord.Color.blurple()):
+    embed = discord.Embed(title=title, description=description, color=color)
+    return embed
