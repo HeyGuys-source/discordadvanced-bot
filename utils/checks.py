ec3e2fcd7242a0c7d45e580567db8c8928c3626e
HeyGuys-source <jurassicdev500@gmail.com>
2025-08-04 03:44:30 +0100

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,8 +1,9 @@
+import discord
 from discord.ext import commands
 
 def has_permissions(**perms):
     async def predicate(ctx):
         if not ctx.guild:
             return False
-        return ctx.author.guild_permissions >= discord.Permissions(**perms)
+        return ctx.author.guild_permissions.is_superset(discord.Permissions(**perms))
     return commands.check(predicate)
