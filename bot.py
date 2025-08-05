4342bcf8aebfff0c20fe5eb55fdf9a4ddb56ae10
HeyGuys-source <jurassicdev500@gmail.com>
2025-08-05 18:33:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,19 +8,6 @@ from aiohttp import web
 from database import Database
 from config import Config
 
-async def keep_alive():
-    async def handle(request):
-        return web.Response(text="Bot is alive and kicking!")
-
-    app = web.Application()
-    app.router.add_get('/', handle)
-
-    port = int(os.environ.get('PORT', 8080))
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', port)
-    await site.start()
-
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
