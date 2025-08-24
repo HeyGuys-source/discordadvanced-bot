086c1817c1969f5754759b484684dc358b76d448
HeyGuys-source <jurassicdev500@gmail.com>
2025-08-23 20:42:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# keep_alive.py
 import logging
 from utils.health_api import HealthAPI
 from utils.error_handler import setup_error_handler
@@ -10,16 +9,16 @@ from aiohttp import web
 from database import Database
 from config import Config
 # from render_health_setup import setup_render_health_monitoring
-
+# Create bot instance
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    print('Bot is ready!')
     
-    # Add this line:
-    await setup_render_health_monitoring(bot)
-    
-    print('Health monitoring active!')
-
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
